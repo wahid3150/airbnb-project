9112b4b26a73fe7a8a7065c12444b54ba8956f36
unknown <wahidali3150@gmail.com.>
2024-12-18 15:10:43 +0500

    added schema & tested

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const colors = require("colors");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const List = require("./models/list");
+
+const app = express();
+
+app.use(morgan("dev"));
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+
+main()
+  .then(() => {
+    console.log("Connected to Database".bgGreen.white);
+  })
+  .catch(() => {
+    console.log("Failed to connect to database".bgRed.bold);
+  });
+
+async function main() {
+  await mongoose.connect(uri);
+}
+
+app.get("/", (req, res) => {
+  res.send("Welcome to home page");
+});
+
+app.get("/testListing", async (req, res) => {
+  let sampleList = new List({
+    title: "My new bedroom",
+    description: "New hostel living bedroom",
+    price: 1000,
+    location: "Peshawar",
+    country: "Pakistan",
+  });
+  await sampleList.save();
+  console.log("Sample was saved");
+  res.send("Testing Successful");
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`.bgBlue.white);
+});
---
+++ models/list.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  image: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1505693416388-ac5ce068fe85?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    set: (v) =>
+      v === ""
+        ? "https://images.unsplash.com/photo-1505693416388-ac5ce068fe85?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        : v,
+  },
+  price: Number,
+  location: String,
+  country: String,
+});
+
+const List = mongoose.model("List", listingSchema);
+module.exports = List;
