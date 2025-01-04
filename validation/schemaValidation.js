06fa947543767894dc056dbacc53eed370e743a6
unknown <wahidali3150@gmail.com.>
2025-01-04 23:16:46 +0500

    Add validation rules using Joi for reviews and update listing validation

--- validation/schemaValidation.js
+++ validation/schemaValidation.js
@@ -30,4 +30,19 @@ const listingSchema = Joi.object({
   }),
 });
 
-module.exports = listingSchema;
+const reviewSchema = Joi.object({
+  review: Joi.object({
+    rating: Joi.number().min(1).max(5).required().messages({
+      "number.base": "Rating must be a number",
+      "number.min": "Rating must be at least 1",
+      "number.max": "Rating cannot exceed 5",
+      "any.required": "Rating is required",
+    }),
+    comment: Joi.string().required().messages({
+      "string.empty": "Comment cannot be empty",
+      "any.required": "Comment is required",
+    }),
+  }),
+});
+
+module.exports = { listingSchema, reviewSchema };
