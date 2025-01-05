cee27288b75a905560df037c20de7c548eafb52d
unknown <wahidali3150@gmail.com.>
2025-01-06 00:30:05 +0500

    Require session and flash also add middlwares for the entire project

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError");
-
 const listings = require("./routes/listings");
 const reviews = require("./routes/reviews");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const app = express();
 app.engine("ejs", ejsMate);
@@ -21,6 +22,26 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+const sessionOptions = {
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
+  next();
+});
 
 const uri = process.env.MONGO_URI;
 
