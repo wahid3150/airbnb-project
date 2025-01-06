f82d328413628d55e1d6ade45e49629b3e19a9ff
unknown <wahidali3150@gmail.com.>
2025-01-06 20:38:56 +0500

    Add signup/login routes and functionality

---
+++ routes/user.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const User = require("../models/user");
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+const router = express.Router();
+
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
+});
+
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ username, email });
+      const registeredUser = await User.register(newUser, password);
+      console.log(registeredUser);
+      req.flash("success", "Welcome to airbnb");
+      res.redirect("/listings");
+    } catch (error) {
+      req.flash("error", error.message);
+      res.redirect("/signup");
+    }
+  })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    req.flash("success", "Welcome back to Airbnb!");
+    res.redirect("/listings");
+  }
+);
+module.exports = router;
