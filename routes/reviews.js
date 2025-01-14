0b3af8c26fb69f07b6c17f56b0c56f0f03eea365
unknown <wahidali3150@gmail.com.>
2025-01-15 01:12:40 +0500

    Add validation to check isLoggedIn/isReviewAuthor

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,26 +3,17 @@ const router = express.Router({ mergeParams: true });
 const List = require("../models/list");
 const Review = require("../models/review");
 const wrapAsync = require("../utils/wrapAsync");
-const ExpressError = require("../utils/ExpressError");
-const { reviewSchema } = require("../validation/schemaValidation");
-
-const validateReview = (req, res, next) => {
-  let { error } = reviewSchema.validate(req.body);
-  if (error) {
-    let errMsg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(404, errMsg);
-  } else {
-    next();
-  }
-};
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../middleware");
 
 //Post request
 router.post(
   "/",
+  isLoggedIn,
   validateReview,
   wrapAsync(async (req, res) => {
     let list = await List.findById(req.params.id);
     let newReview = new Review(req.body.review);
+    newReview.author = req.user._id;
     list.reviews.push(newReview);
     await newReview.save();
     await list.save();
@@ -34,6 +25,8 @@ router.post(
 //Delete Review Route
 router.delete(
   "/:reviewId",
+  isLoggedIn,
+  isReviewAuthor,
   wrapAsync(async (req, res) => {
     let { id, reviewId } = req.params;
     await List.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
